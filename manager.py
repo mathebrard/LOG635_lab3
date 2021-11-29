e25ea1c41bb43ad114bb0292ef790c9e6babba19
Simon <simon.benoit.4@ens.etsmtl.ca>
2021-11-29 16:22:26 -0500

    update to manager

--- manager.py
+++ manager.py
@@ -10,9 +10,11 @@ engine = CrimeInference()
 class Manager:
 
     def __init__(self):
-        print("new manager who this")
         self.usedSentences = []
         self.crimeTime = 2
+        self.itemsOfInterest = ["le couteau","le revolver","la corde","le tuyau","la matraque","le chandelier"]
+        self.peopleOfInterest = ["Mustard","Peacock", "Scarlet", "Plum", "White","Green"]
+        self.rooms = ["le salon","la cuisine","le bureau","le studio","la bibliothèque","la cave"]
 
 
     # Cette fonction retourne le format d'une expression logique de premier ordre
@@ -32,27 +34,26 @@ class Manager:
         return sent   
 
     def ask_question(self):
-        itemsOfInterest = ["le couteau","le revolver","la corde","le tuyau","la matraque","le chandelier"]
-        peopleOfInterest = ["Mustard","Peacock", "Scarlet", "Plum", "White","Green"]
-        rooms = ["le salon","la cuisine","le bureau","le studio","la bibliothèque","la cave"]
         structure = randrange(3)
         
         if structure == 0:
             print("This is structure 0")
-            sentence = "Où était "+ itemsOfInterest[randint(0,5)]+" à "+str(self.crimeTime)+"h?"
+            sentence = "Où est "+ self.itemsOfInterest[randint(0,5)]+"?"
+            question = 'grammars/arme_piece.fcfg'
             if self.checkSentence(sentence):
-                self.writeAnswer(sentence)
+                self.writeAnswer(sentence,question)
 
         elif structure == 1:
             print("This is structure 1")
-            sentence = "Où était "+ peopleOfInterest[randint(0,5)]+" à "+str(self.crimeTime)+"h?"
+            sentence = "Où était "+ self.peopleOfInterest[randint(0,5)]+" à "+str(self.crimeTime)+"h?"
+            question = 'grammars/personne_piece_heure.fcfg'
             if self.checkSentence(sentence):
-                self.writeAnswer(sentence)
+                self.writeAnswer(sentence,question)
 
         elif structure == 2:
             print("This is sctucture 2")
-            itemsOfInterest=itemsOfInterest+peopleOfInterest
-            sentence = "Est-ce que "+itemsOfInterest[randint(0,11)]+" était dans "+rooms[randint(0,5)]+" à "+str(self.crimeTime)+"h?"
+            itemsOfInterest=self.itemsOfInterest+self.peopleOfInterest
+            sentence = "Est-ce que "+itemsOfInterest[randint(0,11)]+" était dans "+self.rooms[randint(0,5)]+" à "+str(self.crimeTime)+"h?"
             if self.checkSentence(sentence):
                 self.writeAnswer(sentence)
 
@@ -65,17 +66,16 @@ class Manager:
         self.usedSentences.append(sentence)
         return True
 
-    def writeAnswer(self,sentence):
+    def writeAnswer(self,sentence,structure):
         print(sentence)
         input("Press any key to read answer.txt")
         with codecs.open('answer.txt','r','utf8') as f:
             lines = f.readlines()
             if len(lines) > 1 or lines[0].endswith("\n"):
                 print("s'il vous plait écrire seulement sur une ligne")
-                self.writeAnswer(sentence)
+                self.writeAnswer(sentence,structure)
             answer = [lines[0]]
-            
-            self.to_fol(answer, 'grammars/arme_piece.fcfg')
+            self.to_fol(answer, structure)
 
 
 
